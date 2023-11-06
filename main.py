903905a86f9abfb599eed022381c4f8b646e3e4b
Garech-mas <hamback.ll9i@gmail.com>
2023-11-07 00:00:40 +0900

    エフェクト種類を大幅追加(動作確認してない)、バージョン更新

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #####################################################################################
-#               RPP to EXO ver 2.05.2                                               #
-#                                                                       2023/11/06  #
+#               RPP to EXO ver 2.05.3                                               #
+#                                                                       2023/11/07  #
 #       Original Written by Maimai (@Maimai22015/YTPMV.info)                        #
 #       Forked by Garech (@Garec_)                                                  #
 #                                                                                   #
@@ -24,7 +24,7 @@ import rpp2exo
 from rpp2exo import Rpp, Exo
 from rpp2exo.dict import *
 
-R2E_VERSION = '2.05.2'
+R2E_VERSION = '2.05.3'
 
 rpp_cl = Rpp("")
 mydict = mydict
